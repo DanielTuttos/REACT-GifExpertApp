aa9fcb43d3714671e4d1176c51a4d06afe9cf305
DanielTuttos <tutorialesdanielromero@gmail.com>
2020-07-14 22:01:15 -0500

    Docs Add

---
+++ docs/precache-manifest.b2f8963129fcb9661f766089881ee496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7160ffd8a98f7877844cbde07247a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d4505368a27cfc1928a",
+    "url": "/static/css/main.f5f43254.chunk.css"
+  },
+  {
+    "revision": "99e680c708e8e4e99ff6",
+    "url": "/static/js/2.086f22a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.086f22a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4505368a27cfc1928a",
+    "url": "/static/js/main.541931b5.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
